f6eaef1086746c8a8ee3457d641b50d6279bf632
Anderson Queiroz <anderson.queiroz@grupoboticario.com.br>
2024-02-14 23:06:52 -0300

    refactor: remove unnecessary code and comments

--- REGISTER_MS/src/schemas/deleteAllUsers.schema.ts
+++ REGISTER_MS/src/schemas/deleteAllUsers.schema.ts
@@ -3,7 +3,6 @@ import * as yup from 'yup'
 const secret = process.env.DELETE_SECRET ?? 'delete-sem-where'
 
 const deleteAllUsersSchema = yup.object({
-  // needs to be equal to the env.DELETE_SECRET
   rootSecret: yup.string().required().equals([secret], 'get out of here')
 })
 
